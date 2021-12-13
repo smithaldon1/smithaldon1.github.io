d5f52cc86d7cb417ba6eb08fe344c94247217430
Aldon Smith <smithaldon@gmail.com>
2021-12-13 12:09:20 -0500

    theme update

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,5 +27,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+THEME = "pure"
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
